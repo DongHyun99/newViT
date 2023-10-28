0557f3536d110d39f2503c016ef6874e43f98502
donghyun <mpolio2@kyonggi.ac.kr>
2023-10-28 12:21:54 +0000

    ✨ update patch embedding class

--- patch_embedding.py
+++ patch_embedding.py
@@ -3,6 +3,8 @@ import torch.nn.functional as F
 import torch
 import cv2
 import numpy as np
+import torchsummary
+import math
 
 from torch import Tensor
 from torchvision.transforms import Compose, ToTensor
@@ -12,19 +14,34 @@ from einops import repeat, rearrange
 from PIL import Image
 
 class PatchEmbedding(nn.Module):
-    def __init__(self, in_channels: int = 3, patch_size: int = 16, emb_size: int = 3, img_size: int = 512, dimension: bool = False): # 768
-        self.patch_size = patch_size
+    def __init__(self, in_channels: int = 3, patch_size: int = 16, emb_size: int = 768, img_size: int = 256, stage: int = 1):
+        repeat_num = int(math.log2(img_size//patch_size))
         super().__init__()
-        
         self.projection = nn.Sequential(
             # using a conv layer instead of a linear one -> performance gains
-            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),)
-            #Rearrange('b e (h) (w) -> b (h w) e'),)
-                    
+            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),
+            Rearrange('b (c e) h w -> b c e h w', c=1),
+            *[nn.Sequential(
+                Rearrange(f'b c e (h1 h2) w -> b c e h2 (h1 w)', h1=2),
+                Rearrange(f'b c e h (w1 w2) -> b (c w1) e h w2 ', w1=4))
+              for _ in range(0, repeat_num)],
+            Rearrange('b c e h w -> b (c h w) e'))
+        
+        self.cls_token = nn.Parameter(torch.randn(1,1, emb_size))
+            
     def forward(self, x: Tensor) -> Tensor:
+        b, _, _, _ = x.shape
         x = self.projection(x)
+        #cls_tokens = repeat(self.cls_token, '() n e -> b n e', b=b)
+        # prepend the cls token to the input
+        #x = torch.cat([cls_tokens, x], dim=1)
+        # add position embedding
         return x
 
+img_size = 512
+torchsummary.summary(PatchEmbedding(img_size=512, patch_size=128).cuda(), (3, img_size, img_size), batch_size=4)
+
+'''
 patch_size = 1
 emb_size = 3
 img_size = 512
@@ -40,62 +57,17 @@ filter = filter.unsqueeze(0).unsqueeze(0)
 filter = repeat(filter, 'o i w h ->(dim1 o) i w h', dim1 = emb_size)
 
 x = F.conv2d(img, filter, stride = (patch_size, patch_size), groups=emb_size)
+x = rearrange(x, 'b (c e) h w -> b c e h w', c=1)
 
-value = 1
+value = 2
 
 for idx in range(1,value+1):
-    x = rearrange(x, f'b {" ".join(f"c{c}" for c in range(idx))} (h1 h2) w -> b {" ".join(f"c{c}" for c in range(idx))} h2 (h1 w)', h1=2) # 1 cycle
-    if idx != value:
-        x = rearrange(x, f'b {" ".join(f"c{c}" for c in range(idx))} h (w1 w2) -> b {" ".join(f"c{c}" for c in range(idx))} w1 h w2 ', w1=4) # new channel
+    x = rearrange(x, f'b c e (h1 h2) w -> b c e h2 (h1 w)', h1=2) # 1 cycle
+    x = rearrange(x, f'b c e h (w1 w2) -> b (c w1) e h w2 ', w1=4) # new channel
 
-for idx in range(value, 1, -1):
-    x = rearrange(x, f'b {" ".join(f"c{c}" for c in range(idx))} h w -> b {" ".join(f"c{c}" for c in range(idx-1))} h ({f"c{idx-1}"} w)') # channel reduction
-
-print(x.size())
-
-x = rearrange(x, 'b c h w -> h w (c b)') * 255.0 / patch_size**2
+x = rearrange(x, 'b c e h w -> h (c w) (e b)') * 255.0 / patch_size**2
+print(x.shape)
 x = np.array(x.detach(), dtype=np.uint8)
 x = cv2.cvtColor(x, cv2.COLOR_RGB2BGR)
 cv2.imwrite('32.png', x)
-
-'''
-x = rearrange(x, 'b c (h1 h2) w -> b c h2 (h1 w)', h2=img_size//patch_size//2) # 1 cycle
-x = rearrange(x, 'b c1 h (c2 w2) -> b c1 c2 h w2 ', w2 =img_size//patch_size//2) # new channel w1
-x = rearrange(x, 'b c1 c2 (h1 h2) w -> b c1 c2 h2 (h1 w)', h2=img_size//patch_size//2//2) # 2cycle
-x = rearrange(x, 'b c1 c2 h w -> b c1 h (c2 w)') # channel reduction
-'''
-
-'''
-patch_size = 1
-emb_size = 712
-img_size = 512
-
-img = Image.open('data/cat.png').convert('RGB')
-transform = Compose([ToTensor()])
-
-img = transform(img)
-img = img.unsqueeze(0)
-
-filter = torch.ones((3, patch_size, patch_size))
-filter = filter.unsqueeze(0).unsqueeze(0)
-filter = repeat(filter, 'o i w h ->(dim1 o) i w h', dim1 = emb_size)
-
-x = F.conv2d(img, filter, stride = (patch_size, patch_size))
-
-value = 4
-
-for idx in range(1,value+1):
-    x = rearrange(x, f'b {" ".join(f"c{c}" for c in range(idx))} (h1 h2) w -> b {" ".join(f"c{c}" for c in range(idx))} h2 (h1 w)', h1=2) # 1 cycle
-    if idx != value:
-        x = rearrange(x, f'b {" ".join(f"c{c}" for c in range(idx))} h (w1 w2) -> b {" ".join(f"c{c}" for c in range(idx))} w1 h w2 ', w1=4) # new channel
-
-for idx in range(value, 1, -1):
-    x = rearrange(x, f'b {" ".join(f"c{c}" for c in range(idx))} h w -> b {" ".join(f"c{c}" for c in range(idx-1))} h ({f"c{idx-1}"} w)') # channel reduction
-
-print(x.size())
-
-x = rearrange(x, 'b c h w -> h w (c b)') * 255.0 / patch_size**2
-x = np.array(x.detach(), dtype=np.uint8)
-x = cv2.cvtColor(x, cv2.COLOR_RGB2BGR)
-cv2.imwrite('32.png', x)
-'''
+# '''
